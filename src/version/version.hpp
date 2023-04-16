5f575deebd6b82e70e98d2093b06250460bb8446
vikdema <viktordeminoff@gmail.com>
2023-04-16 14:09:39 +0300

    add version

--- src/version/version.hpp
+++ src/version/version.hpp
@@ -1,9 +1,2 @@
-/***** spin: version.h *****/
-
-/*
- * This file is part of the public release of Spin. It is subject to the
- * terms in the LICENSE file that is included in this source directory.
- * Tool documentation is available at http://spinroot.com
- */
-
-#define SpinVersion	"Spin Version 6.5.2 -- 13 October 2022"
+#pragma once
+#define SpinVersion "1.0.0"
